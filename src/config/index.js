12c44fc4ea8478f00515804bcbd04effe376b5a2
cjh <1418503647@qq.com>
2018-05-05 17:10:17 +0800

    update API URL

--- src/config/index.js
+++ src/config/index.js
@@ -5,13 +5,13 @@ let apiDomain
 let apiDomain2
 if (process.env.NODE_ENV === 'production') {
     imgDomain = 'http://120.79.29.47'
-    apiDomain = 'http://php.yunser.com'
-    apiDomain2 = 'http://node.api.yunser.com'
+    apiDomain = 'https://phpapi.yunser.com'
+    apiDomain2 = 'https://nodeapi.yunser.com'
 } else {
     imgDomain = 'http://120.79.29.47'
-    apiDomain = 'http://php.api.yun.com'
-    // apiDomain = 'http://php.yunser.com'
-    apiDomain2 = 'http://localhost:1026'
+    apiDomain = 'https://phpapi.yunser.com'
+    // apiDomain2 = 'http://localhost:1026'
+    apiDomain2 = 'https://nodeapi.yunser.com'
 }
 
 module.exports = {
