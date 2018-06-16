fcc1de9444e22b025614d7fda19422ff558136e6
cjh <1418503647@qq.com>
2018-06-16 21:33:04 +0800

    域名注册支持多域名后缀

--- src/views/Domain.vue
+++ src/views/Domain.vue
@@ -20,6 +20,20 @@
                 </tr>
             </table>
         </ui-article>
+        <ui-float-button class="float-button" icon="more_horiz" @click="open = true"/>
+        <ui-drawer class="setting-box" right :open="open" @close="toggle()">
+            <ui-appbar title="设置">
+                <ui-icon-button icon="close" slot="left" @click="open = false" />
+            </ui-appbar>
+            <div class="body">
+                <ul class="type-list">
+                    <li class="item" v-for="t in types">
+                        <ui-checkbox name="group" :nativeValue="t" 
+                            v-model="selected" :label="t" class="demo-checkbox"/>
+                    </li>
+                </ul>
+            </div>
+        </ui-drawer>
     </my-page>
 </template>
 
@@ -32,14 +46,26 @@
                 loading: false,
                 domain: '',
                 result: '',
+                open: false,
+                types: [
+                    'com', 'cn', 'xin', 'net', 'top', '在线',
+                    'xyz', 'wang', 'shop', 'site', 'club', 'cc',
+                    'fun', 'online', 'biz', 'red', 'ink', 'ltd',
+                    'mobi', 'info', 'org', 'com.cn', 'net.cn', 'org.cn',
+                    'gov.cn', 'name', 'vip', 'pro', 'work', 'tv',
+                    'kim', 'group', 'tech', 'store', 'ren', 'ink',
+                    'pub', 'live', 'wiki', 'design', '中文网', '我爱你',
+                    '中国', '网址', '网店', '公司', '网络', '集团'
+                ],
                 domains: [],
+                selected: ['com', 'cn', 'net', 'top', 'org', 'wang', 'vip'],
                 page: {
                     menu: [
-                        {
-                            type: 'icon',
-                            icon: 'help',
-                            to: '/whois/help'
-                        }
+                        // {
+                        //     type: 'icon',
+                        //     icon: 'help',
+                        //     to: '/whois/help'
+                        // }
                     ]
                 }
             }
@@ -55,6 +81,9 @@
             // this.query()
         },
         methods: {
+            toggle () {
+               this.open = !this.open
+            },
             deal(content) {
                 content = content.split('>>>')[0]
                 let arr = content.split('\n')
@@ -79,8 +108,18 @@
                 }
                 // this.loading = true
                 let prefix = this.domain.split('.')[0]
-                let types = ['com', 'cn', 'net', 'top', 'org', 'wang', 'vip']
+                let type = this.domain.split('.')[1]
+                let types = this.selected
+                types = types.sort((a, b) => {
+                    if (a === type) {
+                        return -1
+                    } else if (b === type) {
+                        return 1
+                    }
+                    return 0
+                })
                 this.result = true
+                this.domains = []
                 // this.domains = []
                 for (let i = 0; i < types.length; i++) {
                     console.log('循环', this.domains)
@@ -94,10 +133,10 @@
                         this.$http.get('/whois.php?domain=' + prefix + '.' + types[i]).then(
                         response => {
                             let data = response.data
-                            if (data.includes('NOT FOUND')) {
-                                this.domains[i].registered = false
-                            } else {
+                            if (data.includes('Domain Name')) {
                                 this.domains[i].registered = true
+                            } else {
+                                this.domains[i].registered = false
                             }
                             this.domains[i].loading = false
                         },
@@ -114,6 +153,13 @@
 </script>
 
 <style lang="scss" scoped>
+    .type-list {
+        overflow: hidden;
+        .item {
+            float: left;
+            width: 160px;
+        }
+    }
     .btns {
         margin-top: 16px;
         margin-bottom: 16px;
@@ -123,5 +169,17 @@
     }
     .result {
         margin-top: 24px;
+    }
+    .float-button {
+        position: fixed;
+        right: 24px;
+        bottom: 24px;
+    }
+    .setting-box {
+        z-index: 10000;
+        width: 100%;
+        .body {
+            padding: 16px;
+        }
     }
 </style>
